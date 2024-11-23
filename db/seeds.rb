eb50e7d419c662ec90fab6a366d17044270bfaa2
Kubatron 117 <kubatron117@gmail.com>
2024-11-23 10:53:38 +0100

    Add missing index

--- db/seeds.rb
+++ db/seeds.rb
@@ -9,16 +9,16 @@
 #   end
 
 
-# Account.create!(
-#   first_name: 'John',
-#   last_name: 'Doe',
-#   user_agreement: true,
-#   date_of_agreement: Time.current,
-#   date_of_birth: '1954-01-01',
-#   estimated_lifespan: '2034-01-01',
-#   email: "kubatron117@gmail.com",
-#   password: 'Password123@'
-# )
+Account.create!(
+  first_name: 'John',
+  last_name: 'Doe',
+  user_agreement: true,
+  date_of_agreement: Time.current,
+  date_of_birth: '1954-01-01',
+  estimated_lifespan: '2034-01-01',
+  email: "kubatron117@gmail.com",
+  password: 'Password123@'
+)
 #
 # Account.create!(
 #   first_name: 'Pepa',
@@ -30,3 +30,22 @@
 #   email: "pepa@novak.local",
 #   password: 'Password123@'
 # )
+
+
+WeeksInLife.create!(
+  start_date: '2024-11-18',
+  end_date: '2024-11-24',
+  week_number: 47,
+  year: 2024,
+  memo: 'Testovací záznam 1',
+  account_id: 1
+)
+
+WeeksInLife.create!(
+  start_date: '2024-11-25',
+  end_date: '2024-12-01',
+  week_number: 48,
+  year: 2024,
+  memo: 'Testovací záznam 2',
+  account_id: 1
+)
