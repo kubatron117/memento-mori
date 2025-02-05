0edbb5800ea5f1240eab961d756dc80348694bb0
Kubatron 117 <kubatron117@gmail.com>
2025-02-05 11:33:14 +0100

    Validate - only update this or previous week

--- app/models/weeks_in_life.rb
+++ app/models/weeks_in_life.rb
@@ -1,3 +1,20 @@
 class WeeksInLife < ApplicationRecord
   belongs_to :account
+
+  validate :allowed_week_update, on: :update
+
+
+  private
+  def allowed_week_update
+    allowed_start_dates = [
+      Date.current.beginning_of_week,
+      Date.current.beginning_of_week - 7.days
+    ]
+
+    unless allowed_start_dates.include?(start_date)
+      errors.add(:base, "Update is only allowed for the current week or the previous week")
+    end
+  end
+
+
 end
