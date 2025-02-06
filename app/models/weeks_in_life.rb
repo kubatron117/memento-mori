0d87a2686df69b4715d00a26998cb609845f12e1
Kubatron 117 <kubatron117@gmail.com>
2025-02-06 13:50:27 +0100

    fix callback

--- app/models/weeks_in_life.rb
+++ app/models/weeks_in_life.rb
@@ -3,11 +3,10 @@ class WeeksInLife < ApplicationRecord
 
   validate :allowed_week_update, on: :update
 
-
-  before_save :count_total_score
-
+  before_save :count_total_score, if: :score_attributes_updated?
 
   private
+
   def allowed_week_update
     allowed_start_dates = [
       Date.current.beginning_of_week,
@@ -20,8 +19,24 @@ class WeeksInLife < ApplicationRecord
   end
 
   def count_total_score
-    self.total_score = self.score_satisfaction + self.score_emotional_balance + self.score_productivity +
-      self.score_relationships + self.score_values_alignment
+    self.total_score = score_satisfaction.to_i +
+      score_emotional_balance.to_i +
+      score_productivity.to_i +
+      score_relationships.to_i +
+      score_values_alignment.to_i
   end
 
+  def score_attributes_updated?
+    score_attrs = %w[
+      score_satisfaction
+      score_emotional_balance
+      score_productivity
+      score_relationships
+      score_values_alignment
+    ]
+
+    score_attrs.any? do |attr|
+      will_save_change_to_attribute?(attr) && self.send(attr).present?
+    end
+  end
 end
