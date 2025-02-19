3d1dbd74c857eb9053f86a0403c78ba83d22bfde
Kubatron 117 <kubatron117@gmail.com>
2025-02-19 10:23:45 +0100

    Fix job - only send email for current week

--- app/jobs/send_week_data_entry_reminder_job.rb
+++ app/jobs/send_week_data_entry_reminder_job.rb
@@ -2,9 +2,17 @@ class SendWeekDataEntryReminderJob < ApplicationJob
   queue_as :default
 
   def perform(*args)
+    current_week = Date.current.cweek
+    current_year = Date.current.year
+
     accounts = Account.joins(:weeks_in_lives)
-                      .where(weeks_in_lives: { memo: [nil, ''] })
-                      .distinct
+                      .where(
+                        weeks_in_lives: {
+                          memo: [nil, ''],
+                          week_number: current_week,
+                          year: current_year
+                        }
+                      ).distinct
 
     accounts.find_each do |account|
       WeeksInLivesMailer.week_reminder(account).deliver_later
